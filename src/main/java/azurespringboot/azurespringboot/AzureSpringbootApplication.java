669ba1297194e9c2955513c6bfc561dcbf9b1117
websergiu <web_sergiu_2006@yahoo.com>
2022-07-01 11:51:51 +0300

    first commit

---
+++ src/main/java/azurespringboot/azurespringboot/AzureSpringbootApplication.java
@@ -0,0 +1,21 @@
+package azurespringboot.azurespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureSpringbootApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "App deployed to Azure"
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringbootApplication.class, args);
+	}
+
+}
